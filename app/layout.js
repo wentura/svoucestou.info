48974e51367c9c33c0bbf741dc903880ffcd9dee
Zbynek Svoboda <wentura17@seznam.cz>
2024-09-17 23:44:09 +0200

    init

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,8 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Podpora from "@/components/podpora";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,10 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` antialiased`}>
+        <Header />
+
         {children}
+        {/* <Podpora /> */}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/kontakt.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default function Kontakt() {
+  return (
+    <section className="relative text-gray-600 body-font">
+      <div className="absolute inset-0 bg-gray-300">
+        <iframe
+          width="100%"
+          height="100%"
+          frameBorder={0}
+          marginHeight={0}
+          marginWidth={0}
+          title="map"
+          scrolling="no"
+          src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+        />
+      </div>
+      <div className="container flex px-5 py-24 mx-auto">
+        <div className="relative z-10 flex flex-col w-full p-8 mt-10 bg-white rounded-lg shadow-md lg:w-1/3 md:w-1/2 md:ml-auto md:mt-0">
+          <h2 className="mb-1 text-lg font-medium text-gray-900 title-font">
+            Feedback
+          </h2>
+          <p className="mb-5 leading-relaxed text-gray-600">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="text-sm leading-7 text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="text-sm leading-7 text-gray-600"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+              defaultValue={""}
+            />
+          </div>
+          <button className="px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
+            Button
+          </button>
+          <p className="mt-3 text-xs text-gray-500">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
