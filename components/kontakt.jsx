a2cb683ffc39e1412bdda670041dcf394419b19d
Zbynek Svoboda <wentura17@seznam.cz>
2024-11-10 12:58:44 +0100

    dalsi prace a nejaky hejblatka

--- components/kontakt.jsx
+++ components/kontakt.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 
 export default function Kontakt() {
   return (
@@ -12,46 +12,22 @@ export default function Kontakt() {
           marginWidth={0}
           title="map"
           scrolling="no"
-          src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-          style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+          src="https://maps.google.com/maps?width=100%&height=600&hl=cz&q=%C4%B0Nebuzely+3+(Svou%20Cestou)&ie=UTF8&t=&z=13&iwloc=B&output=embed"
+          // src="https://maps.google.com/maps?width=100%&height=600&hl=cz&q=%C4%B0Nebuzely+3+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27046.69039827414!2d14.56043130251515!3d50.40239049783139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470be198b4700cb3%3A0x2c58ca874da341c7!2sSvou%20cestou!5e1!3m2!1scs!2scz!4v1729627813623!5m2!1scs!2scz"
+          // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27046.690398274135!2d14.56043130251515!3d50.40239049783139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470be198b4700cb3%3A0x2c58ca874da341c7!2sSvou%20cestou!5e1!3m2!1scs!2scz!4v1729628006515!5m2!1scs!2scz"
+          style={{ filter: "grayscale(1) contrast(1.1) opacity(0.5)" }}
         />
       </div>
-      <div className="container flex px-5 py-24 mx-auto">
-        <div className="relative z-10 flex flex-col w-full p-8 mt-10 bg-white rounded-lg shadow-md lg:w-1/3 md:w-1/2 md:ml-auto md:mt-0">
+      <div className="container flex px-2 py-24 mx-auto">
+        <div className="relative z-10 flex flex-col w-full p-4 mt-4 bg-white rounded-lg shadow-md lg:w-1/3 md:w-1/3 md:ml-0 md:mt-2">
           <h2 className="mb-1 text-lg font-medium text-gray-900 title-font">
-            Feedback
+            Svou Cestou
           </h2>
           <p className="mb-5 leading-relaxed text-gray-600">
-            Post-ironic portland shabby chic echo park, banjo fashion axe
+            Montessori škola a školka
           </p>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="text-sm leading-7 text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="text-sm leading-7 text-gray-600"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full h-32 px-3 py-1 text-base leading-6 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none resize-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-              defaultValue={""}
-            />
-          </div>
-          <button className="px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
-            Button
-          </button>
+
           <p className="mt-3 text-xs text-gray-500">
             Chicharrones blog helvetica normcore iceland tousled brook viral
             artisan.
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        monte: {
+          100: "#f38181ff",
+          // 150: "#fde6e6",
+          150: "#d3f6f2",
+          200: "#625772ff",
+          300: "#a9eee6",
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
