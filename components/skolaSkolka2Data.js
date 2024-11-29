2631fd8a972811f3446e1d732ce7f6a6a2dd49b9
Zbynek Svoboda <wentura17@seznam.cz>
2024-11-29 20:54:57 +0100

    mensi fotky na rozcestnik

--- components/skolaSkolka2Data.js
+++ components/skolaSkolka2Data.js
@@ -2,21 +2,21 @@ export const skolaSkolka2Data = [
   {
     title: "Školka",
     text: "neco o skolce, claim",
-    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732399710/svoucestou.info/skolka.webp",
+    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732909970/svoucestou.info/rozcestnik/1.webp",
 
     url: "/skolka",
   },
   {
     title: "Škola",
     text: "1. až 5. třída",
-    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732399695/svoucestou.info/435094426_973758781420089_8143381376244072342_n.webp",
+    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732909970/svoucestou.info/rozcestnik/2.webp",
     url: "/skola",
   },
   ,
   {
     title: "Škola 12+",
     text: "6. až 9. třída",
-    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732399720/svoucestou.info/438145904_984960676966566_6959591181115972123_n.webp",
+    img: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1732909970/svoucestou.info/rozcestnik/3.webp",
     url: "/skola",
   },
 ];
